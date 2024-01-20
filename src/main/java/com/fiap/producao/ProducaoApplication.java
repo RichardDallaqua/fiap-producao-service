338b2cde93761ef74b8b1357407f52629b67835e
richard <richard.dallaqua@ab-inbev.com>
2024-01-20 16:25:07 -0300

    Name adjustment

--- src/main/java/com/fiap/producao/ProducaoApplication.java
+++ src/main/java/com/fiap/producao/ProducaoApplication.java
@@ -2,13 +2,12 @@ package com.fiap.producao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class LanchoneteApplication {
+public class ProducaoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LanchoneteApplication.class, args);
+        SpringApplication.run(ProducaoApplication.class, args);
     }
 
 }
